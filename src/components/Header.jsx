ff9cacea9491743290a2dcec5cf163ee88061ac1
dpalexandru <alexpoian91@hotmail.com>
2025-08-28 15:24:15 +0200

    feat: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='container bg-light p-3'>
+      <h1>Learn Web devolopment</h1>
+    </div>
+  )
+}
+
+export default Header
